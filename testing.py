9fc85e99a153150fe920d336e50c48065476064a
JCamyre <JWcamry03@gmail.com>
2021-03-01 01:07:23 -0800

    Adding finishing touches.

--- testing.py
+++ testing.py
@@ -1,5 +1,3 @@
-# 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n). I created a 2d list for visualization purposes.
-
 def convert_to_2d(arr: list):
 	n = max(arr)
 	nums = [[i if i <= n and arr[i] > 0 else ' ' for i in range(j, j+10)] for j in range(0, n, 10)]
@@ -8,21 +6,19 @@ def convert_to_2d(arr: list):
 def illustrate_nums(all_nums):
 	for row in all_nums:
 		print(' '.join([str(n).center(4) for n in row]))
-# illustrate_nums(all_nums)
 
-# Maybe too hard to do it properly, so maybe just do it the easy way
-# All multiples of two
 def SieveOfEratosthenes(n: int):
     nums = [True for i in range(1, n+1)]
-    p = 2
+    p = 2 # Start with 2, as we know 1 can not be prime
     while p * p <= n: # This is how the algorithm knows when to stop testing new prime multiples
-        if nums[p]: 
+        if nums[p]: # If the current prime we are looking at doesn't have a smaller multiple
             for i in range(p*2, n, p): # All multiples of p
                 nums[i] = False
-        p += 1
+        p += 1 # Checking next prime in list
     nums[0] = False # Overriding error in the code's algorithm.
     nums[1] = False
-    # Printing numbers
+    # Returning list of all numbers. Primes have their integer value, while nonprimes are all -1.
     return [i if j else -1 for i, j in enumerate(nums)]
 
+# Formatting lists and printing them to display them in an easy to read format. 
 illustrate_nums(convert_to_2d(SieveOfEratosthenes(100)))
